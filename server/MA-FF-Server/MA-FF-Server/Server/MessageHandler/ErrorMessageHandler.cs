98ebb38672ef5ff91c1ed372357614d0e7219d63
thhesp <Thomas_spr@yahoo.de>
2015-04-14 15:28:16 +0200

    added basic handler classes for event and error messages

---
+++ server/MA-FF-Server/MA-FF-Server/Server/MessageHandler/ErrorMessageHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAnalyzer.Server.MessageHandler
+{
+    class ErrorMessageHandler : IObserver<Object>
+    {
+        readonly ConnectionManager _connectionManager;
+        readonly WebsocketConnection _connection;
+
+        public ErrorMessageHandler(ConnectionManager connectionManager, WebsocketConnection connection)
+        {
+            _connectionManager = connectionManager;
+            _connection = connection;
+        }
+
+        public void OnCompleted()
+        {
+            Console.WriteLine("Error Message: Completed");
+        }
+
+        public void OnError(Exception error)
+        {
+            Console.WriteLine("Error message : " + error.Message);
+        }
+
+        public void OnNext(Object omsgIn)
+        {
+            dynamic msgIn = omsgIn;
+
+        }
+    }
+}
---
+++ server/MA-FF-Server/MA-FF-Server/Server/MessageHandler/EventMessageHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAnalyzer.Server.MessageHandler
+{
+    class EventMessageHandler : IObserver<Object>
+    {
+        readonly ConnectionManager _connectionManager;
+        readonly WebsocketConnection _connection;
+
+        public EventMessageHandler(ConnectionManager connectionManager, WebsocketConnection connection)
+        {
+            _connectionManager = connectionManager;
+            _connection = connection;
+        }
+
+        public void OnCompleted()
+        {
+            Console.WriteLine("Event Message: Completed");
+        }
+
+        public void OnError(Exception error)
+        {
+            Console.WriteLine("Event message : " + error.Message);
+        }
+
+        public void OnNext(Object omsgIn)
+        {
+            dynamic msgIn = omsgIn;
+
+            
+        }
+    }
+}
