abeb71934be9f7307c094cad05de0cb669ef3df4
thhesp <Thomas_spr@yahoo.de>
2015-06-01 21:50:26 +0200

    Added Calling file and method to logging

--- server/MA-FF-Server/MA-FF-Server/Util/Logger.cs
+++ server/MA-FF-Server/MA-FF-Server/Util/Logger.cs
@@ -1,13 +1,15 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
+using System.IO;
 
 namespace WebAnalyzer.Util
 {
     class Logger
     {
 
-        public static void Log(String line)
+        public static void Log(String line, [CallerFilePathAttribute]string filePath = "", [CallerMemberName]string memberName = "")
         {
-            Console.WriteLine(Timestamp.GetUnixTimestamp() + ": " + line);
+            Console.WriteLine(Path.GetFileName(filePath) + " - " + memberName + ";" + Timestamp.GetUnixTimestamp() + ": " + line);
         }
     }
 }
