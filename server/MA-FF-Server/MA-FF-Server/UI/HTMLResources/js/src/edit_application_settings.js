714c89e205a749ab3748bb967433d177754a2570
thhesp <Thomas_spr@yahoo.de>
2015-08-16 03:33:05 +0200

    added validations to application settings

--- server/MA-FF-Server/MA-FF-Server/UI/HTMLResources/js/src/edit_application_settings.js
+++ server/MA-FF-Server/MA-FF-Server/UI/HTMLResources/js/src/edit_application_settings.js
@@ -13,6 +13,58 @@ $('#et-receive-ip').val(control.getETReceiveIP());
 $('#et-receive-port').val(control.getETReceivePort());
 
 $('#save-button').click(function () {
+
+    if (validateData()) {
+        saveData();
+    }
+
+});
+
+$('#cancel-button').click(function () {
+    console.log('cancel form...');
+    control.cancel();
+});
+
+function validateData() {
+
+    $('#websocket-port').removeClass("invalid");
+    $('#et-sent-ip').removeClass("invalid");
+    $('#et-sent-port').removeClass("invalid");
+    $('#et-receive-ip').removeClass("invalid");
+    $('#et-receive-port').removeClass("invalid");
+
+    var valid = true;
+
+    if (!validatePort($('#websocket-port').val())) {
+        $('#websocket-port').addClass("invalid");
+        valid = false;
+    }
+
+    if(!validateIP($('#et-sent-ip').val())){
+        $('#et-sent-ip').addClass("invalid");
+        valid = false;
+    }
+
+    if (!validatePort($('#et-sent-port').val())) {
+        $('#et-sent-port').addClass("invalid");
+        valid = false;
+    }
+
+    if(!validateIP($('#et-receive-ip').val())){
+        $('#et-receive-ip').addClass("invalid");
+        valid = false;
+    }
+
+    if (!validatePort($('#et-receive-port').val())) {
+        $('#et-receive-port').addClass("invalid");
+        valid = false;
+    }
+
+    return valid;
+}
+
+function saveData() {
+
     console.log('save settings...');
 
     control.setWSPort($('#websocket-port').val());
@@ -30,9 +82,19 @@ $('#save-button').click(function () {
     control.setETReceivePort($('#et-receive-port').val());
     
     control.saveSettings();
-});
+}
 
-$('#cancel-button').click(function () {
-    console.log('cancel form...');
-    control.cancel();
-});
+function validateIP(string) {
+
+    if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(string)) {
+        return (true);
+    }
+    return (false);
+}
+
+function validatePort(string) {
+    if (/^\d{3,}$/.test(string)) {
+        return (true);
+    }
+    return (false);
+}
