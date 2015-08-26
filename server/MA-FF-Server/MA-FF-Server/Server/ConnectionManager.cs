d05e51c8f5aa8240ba6ac53ad5ab21078e4bad69
thhesp <Thomas_spr@yahoo.de>
2015-08-26 11:04:36 +0200

    rebuild code so that toAdd and toRemove class variables are not necessary anymore

--- server/MA-FF-Server/MA-FF-Server/Server/ConnectionManager.cs
+++ server/MA-FF-Server/MA-FF-Server/Server/ConnectionManager.cs
@@ -21,10 +21,6 @@ namespace WebAnalyzer.Server
 
         private List<WebsocketConnection> _connections = new List<WebsocketConnection>();
 
-        private List<WebsocketConnection> _toAdd = new List<WebsocketConnection>();
-
-        private List<WebsocketConnection> _toRemove = new List<WebsocketConnection>();
-
         public ConnectionManager()
         {
             
@@ -33,8 +29,6 @@ namespace WebAnalyzer.Server
         public void ResetConnections()
         {
             _connections.Clear();
-            _toAdd.Clear();
-            _toRemove.Clear();
         }
 
         public void StartMessageThread()
@@ -53,15 +47,6 @@ namespace WebAnalyzer.Server
         public void StopMessageThread()
         {
             _workMessages = false;
-        }
-        public void AddWebsocketConnection(WebsocketConnection connection)
-        {
-            _toAdd.Add(connection);
-        }
-
-        public void RemoveWebsocketConnection(WebsocketConnection connection)
-        {
-            _toRemove.Add(connection);
         }
 
         public void RequestData(String uniqueId, double leftX, double leftY, double rightX, double rightY)
@@ -84,57 +69,22 @@ namespace WebAnalyzer.Server
 
         private void Broadcast(Message message)
         {
-            checkConnectionQueues();
-
             lock (_connections)
             {
+                _connections.RemoveAll(connection => connection.IsConnected == false);
                 foreach (var connection in _connections)
                 {
-                    if (checkConnection(connection))
-                    {
-                        connection.EnqueueMessage(message);
-                    }
+                    connection.EnqueueMessage(message);
                 }
             }
 
-        }
-
-        private void checkConnectionQueues()
-        {
-            lock (_connections)
-            {
-                foreach (var connection in _toAdd)
-                {
-                    _connections.Add(connection);
-                }
-
-                foreach (var connection in _toRemove)
-                {
-                    _connections.Remove(connection);
-                }
-            }
-
-            _toAdd.Clear();
-            _toRemove.Clear();
-        }
-
-        private Boolean checkConnection(WebsocketConnection connection)
-        {
-            if (!connection.IsConnected)
-            {
-                this.RemoveWebsocketConnection(connection);
-                return false;
-            }
-
-            return true;
         }
 
         private void WorkConnectionMessageQueues()
         {
-            checkConnectionQueues();
-
             lock (_connections)
             {
+                _connections.RemoveAll(connection => connection.IsConnected == false);
                 //Logger.Log("Working through queues... ");
                 foreach (var connection in _connections)
                 {
@@ -145,7 +95,10 @@ namespace WebAnalyzer.Server
 
         public void On_AddConnection(object source, AddConnectionEvent e)
         {
-            this.AddWebsocketConnection(e.Connection);
+            lock (_connections)
+            {
+                _connections.Add(e.Connection);
+            }
         }
     }
 }
