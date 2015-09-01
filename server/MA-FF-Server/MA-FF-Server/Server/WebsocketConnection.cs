68cac0c393b1b1b608dcc7005f0e9cb2c08eae97
thhesp <Thomas_spr@yahoo.de>
2015-09-01 15:22:25 +0200

    improved the code by using a list and not an queue

--- server/MA-FF-Server/MA-FF-Server/Server/WebsocketConnection.cs
+++ server/MA-FF-Server/MA-FF-Server/Server/WebsocketConnection.cs
@@ -21,7 +21,7 @@ namespace WebAnalyzer.Server
 
         private Boolean _established = false;
 
-        private ConcurrentQueue<Message> _messageQueue;
+        private List<Message> _messageQueue;
 
         private Boolean _active = false;
 
@@ -30,7 +30,7 @@ namespace WebAnalyzer.Server
         public WebsocketConnection(WebSocket ws)
         {
             _ws = ws;
-            _messageQueue = new ConcurrentQueue<Message>();
+            _messageQueue = new List<Message>();
         }
 
         public Boolean Established
@@ -67,48 +67,55 @@ namespace WebAnalyzer.Server
             set { _active = value; }
         }
 
-        public ConcurrentQueue<Message> MessageQueue
+        public List<Message> MessageQueue
         {
             get { return _messageQueue; }
         }
 
         public void EnqueueMessage(Message message)
         {
-            this.MessageQueue.Enqueue(message);
+            lock (_messageQueue)
+            {
+                this.MessageQueue.Add(message);
+            }
+            
         }
 
         public void workMessageQueue()
         {
-            //check if messages are too old and remove them
-            removeOldMessages();
-
-            if (Writing || !Active || !this.Established || !this.IsConnected || _messageQueue.Count == 0)
-                return;
-
-            //Logger.Log("Sent Message... current Message Count: " + _messageQueue.Count);
-
-            //sent first message (later in try catch block)
-            Message msg = _messageQueue.ElementAt(0);
-            Writing = true;
-
-            if (msg is SmallDataMessage)
+            lock (MessageQueue)
             {
-                this.Out.OnNext(((SmallDataMessage)msg).ToJson());
-            }
-            else if (msg is DataMessage)
-            {
-                this.Out.OnNext(((DataMessage)msg).ToJson());
-            }
-            else
-            {
-                Logger.Log("Message type unknown!");
-                return;
-            }
+                //check if messages are too old and remove them
+                removeOldMessages();
 
-            Message outMessage;
-            _messageQueue.TryDequeue(out outMessage);
+                if (Writing || !Active || !this.Established || !this.IsConnected || _messageQueue.Count == 0)
+                    return;
 
-            Writing = false;
+                //Logger.Log("Sent Message... current Message Count: " + _messageQueue.Count);
+
+                //sent first message (later in try catch block)
+                Message msg = _messageQueue.ElementAt(0);
+                Writing = true;
+
+                if (msg is SmallDataMessage)
+                {
+                    this.Out.OnNext(((SmallDataMessage)msg).ToJson());
+                }
+                else if (msg is DataMessage)
+                {
+                    this.Out.OnNext(((DataMessage)msg).ToJson());
+                }
+                else
+                {
+                    Logger.Log("Message type unknown!");
+                    return;
+                }
+
+                _messageQueue.Remove(msg);
+
+                Writing = false;
+            }
+            
         }
 
         private void removeOldMessages()
@@ -116,24 +123,8 @@ namespace WebAnalyzer.Server
             if (_messageQueue.Count == 0)
                 return;
 
-            Message msg = _messageQueue.ElementAt(0);
             String currentTimestamp = Timestamp.GetMillisecondsUnixTimestamp();
-            while ((long.Parse(currentTimestamp) - long.Parse(msg.Timestamp)) > Properties.Settings.Default.DataTimeout)
-            {
-                // remove first element
-                Message outMessage;
-                _messageQueue.TryDequeue(out outMessage);
-                if (_messageQueue.Count > 0)
-                {
-                    // get new first element
-                    msg = _messageQueue.ElementAt(0);
-                }
-                else
-                {
-                    break;
-                }
-
-            }
+            _messageQueue.RemoveAll(msg => ((long.Parse(currentTimestamp) - long.Parse(msg.Timestamp)) > Properties.Settings.Default.DataTimeout));
         }
     }
 }
